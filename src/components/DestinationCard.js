e7d44ae5b72571378175dba6af42b4ce7191b47c
Emmalyn Grace Buma-a <emmalyngracebumaa@gmail.com>
2022-05-16 09:54:13 +0800

    hid price

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -10,19 +10,18 @@ export default function DestinationCard ({destinationProp}) {
 	console.log(destinationProp);
 	console.log(typeof destinationProp);
 
-	const {name, link, description, price, _id} = destinationProp
+	const {name, link, description, _id} = destinationProp
 
 	return(
 		<Card className="mainCard mt-3 mb-3">
 			<Card className="cardDestination">
-				<Card.Body> 	
+				<Card.Body className="cardBody"> 	
 					<Card.Title className="cardText" >{name}</Card.Title>	
 					<Card.Subtitle className="cardImage">
 						<img src = {link} className="destinationPicture" alt="Destination Pic" />
 					</Card.Subtitle>					
 					<Card.Text className="cardText p-3"> {description}</Card.Text>
-					<Card.Subtitle className="cardText">Price</Card.Subtitle>
-					<Card.Text className="cardText" > Php {price}</Card.Text>
+		
 					<Button className="destinationButton" variant="primary" as={Link} to={`/destinations/${_id}`}>See Details</Button>
 				</Card.Body>
 			</Card>
