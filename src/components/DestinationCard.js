7734687e710df1d1ce88456ca1ed138ce65d38e0
Emmalyn Grace Buma-a <emmalyngracebumaa@gmail.com>
2022-05-16 09:26:38 +0800

    aligned button right

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -15,15 +15,15 @@ export default function DestinationCard ({destinationProp}) {
 	return(
 		<Card className="mainCard mt-3 mb-3">
 			<Card className="cardDestination">
-				<Card.Body className="cardBody"> 	
-					<Card.Title>{name}</Card.Title>	
+				<Card.Body> 	
+					<Card.Title className="cardText" >{name}</Card.Title>	
 					<Card.Subtitle className="cardImage">
 						<img src = {link} className="destinationPicture" alt="Destination Pic" />
 					</Card.Subtitle>					
-					<Card.Text className="p-3"> {description}</Card.Text>
-					<Card.Subtitle>Price</Card.Subtitle>
-					<Card.Text> Php {price}</Card.Text>
-					<Button variant="primary" as={Link} to={`/destinations/${_id}`}>See Details</Button>
+					<Card.Text className="cardText p-3"> {description}</Card.Text>
+					<Card.Subtitle className="cardText">Price</Card.Subtitle>
+					<Card.Text className="cardText" > Php {price}</Card.Text>
+					<Button className="destinationButton" variant="primary" as={Link} to={`/destinations/${_id}`}>See Details</Button>
 				</Card.Body>
 			</Card>
 		</Card>
